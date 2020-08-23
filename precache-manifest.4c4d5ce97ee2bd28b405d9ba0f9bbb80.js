5dbf7017c1c8f00b9ea3dd4108b1e147c88c7895
Pedro Navarrete <pnavarrete@harding.edu>
2020-08-23 02:14:31 -0600

    Updates

---
+++ precache-manifest.4c4d5ce97ee2bd28b405d9ba0f9bbb80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f6a5b452897a206cc8803e789e899e4",
+    "url": "/SeedsOfHopeReact/index.html"
+  },
+  {
+    "revision": "0298bc6793b508b8010b",
+    "url": "/SeedsOfHopeReact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fbad55fd76611686a1e5",
+    "url": "/SeedsOfHopeReact/static/js/2.941a33ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SeedsOfHopeReact/static/js/2.941a33ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0298bc6793b508b8010b",
+    "url": "/SeedsOfHopeReact/static/js/main.e9fcec55.chunk.js"
+  },
+  {
+    "revision": "c8507ee528ea0b35966a",
+    "url": "/SeedsOfHopeReact/static/js/runtime-main.a62f0231.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SeedsOfHopeReact/static/media/logo.5d5d9eef.svg"
+  }
+]);
